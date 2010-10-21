317223e7198e1bf283bcce6a401a7eaf80e3bcf1
Icing.Seeed <Icing.Seeed@759694c5-278e-ff0b-ab78-40d1101df0e0>
2010-10-21 11:39:59 +0000

    Make the code as small as possible to support Talking Breathalyzer

---
+++ branches/arduino code/musicTalkingBreathalyzer/newSDLib.cpp
@@ -0,0 +1,56 @@
+#include <SdFat.h>
+#include <SdFatUtil.h>
+#include "newSDLib.h"
+#include "vs10xx.h"
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+SdFile file;
+
+void error_P(const char* str) {
+  PgmPrint("error: ");
+  SerialPrintln_P(str);
+  if (card.errorCode()) {
+    PgmPrint("SD error: ");
+    Serial.print(card.errorCode(), HEX);
+    Serial.print(',');
+    Serial.println(card.errorData(), HEX);
+  }
+  while(1);
+}
+
+void initialSDCard()
+{
+  // initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
+  // breadboards.  use SPI_FULL_SPEED for better performance.
+  if (!card.init(SPI_HALF_SPEED)) error("card.init failed");
+
+  // initialize a FAT volume
+  if (!volume.init(&card)) error("volume.init failed");
+
+  // open the root directory
+  if (!root.openRoot(&volume)) error("openRoot failed");
+
+}
+
+void openFile(char *fileName)
+{
+  if (file.open(&root, fileName, O_READ)) {
+    Serial.write(fileName);
+    Serial.println(" opened");
+  }
+  else{
+    error("open file failed");
+  }
+}
+
+int readFile(byte *buffer, int len)
+{
+  int readLen = 0;
+  readLen = file.read(buffer,len);
+  return readLen;
+}
+
+
+
---
+++ branches/arduino code/musicTalkingBreathalyzer/newSDLib.h
@@ -0,0 +1,17 @@
+#ifndef NEWSDLIB_H
+#define NEWSDLIB_H
+
+extern Sd2Card card;
+extern SdVolume volume;
+extern SdFile root;
+extern SdFile file;
+
+// store error strings in flash to save RAM
+#define error(s) error_P(PSTR(s))
+
+void initialSDCard();
+void openFile(char *fileName);
+int readFile(byte *buffer, int len);
+void error_P(const char* str) ;
+#endif
+
