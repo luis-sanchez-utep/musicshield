47f7851f2edc906c988d343498e06a2851de8f30
Icing.Seeed <Icing.Seeed@759694c5-278e-ff0b-ab78-40d1101df0e0>
2010-10-21 13:40:43 +0000

    Add copyright info and file descriptions to the project.

--- branches/arduino code/musicTalkingBreathalyzer/vs10xx.cpp
+++ branches/arduino code/musicTalkingBreathalyzer/vs10xx.cpp
@@ -1,9 +1,27 @@
-/** \file vs10xx.c
- * Functions for interfacing with the mp3 player chip.
- * \todo safe rewind
- * \todo VS1003 WMA "wma-bytes-left" variable adjustment at ff/rew
- */
- 
+// file vs10xx.c
+// Functions for interfacing with the mp3 player chip.
+//
+ //  For more details about the product please check http://www.seeedstudio.com/depot/
+//  Copyright (c) 2010 seeed technology inc.
+//  Author: Icing Chang
+//  Version: 1.0
+//  Time: Oct 21, 2010
+//  Changing records:
+//    
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 #include <WProgram.h>
 #include "vs10xx.h"
 
